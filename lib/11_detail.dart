3cff13c2bcf5ded679b1e65eb3eb6a4e28155f3f
pramepree <pramepree159@gmail.com>
2023-12-14 21:27:48 +0700

    upupupupup

--- lib/11_detail.dart
+++ lib/11_detail.dart
@@ -12,7 +12,8 @@ class ShowDetailAll extends StatefulWidget {
 
 class _ShowDetailAllState extends State<ShowDetailAll> {
   Map postData = {};
-
+  List<String> image_post = [];
+  final PageController _pageController = PageController();
   @override
   void initState() {
     super.initState();
@@ -28,6 +29,7 @@ class _ShowDetailAllState extends State<ShowDetailAll> {
         setState(() {
           postData =
               Map<String, dynamic>.from(databaseEvent.snapshot.value as Map);
+          image_post = List<String>.from(postData['imageUrls'] ?? []);
         });
       }
     }).catchError((error) {
@@ -35,12 +37,64 @@ class _ShowDetailAllState extends State<ShowDetailAll> {
     });
   }
 
+  Widget _buildImageSlider() {
+    return image_post.isEmpty
+        ? SizedBox.shrink()
+        : Column(
+            children: [
+              SizedBox(
+                height: 200, // Set your desired height for the image area
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: image_post.length,
+                  itemBuilder: (context, index) {
+                    return Image.network(
+                      image_post[index],
+                      fit: BoxFit.cover,
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                  height:
+                      5), // Adjust the size of this SizedBox as needed for spacing
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back_ios, size: 30),
+                    onPressed: () {
+                      if (_pageController.page! > 0) {
+                        _pageController.previousPage(
+                          duration: Duration(milliseconds: 200),
+                          curve: Curves.easeIn,
+                        );
+                      }
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.arrow_forward_ios, size: 30),
+                    onPressed: () {
+                      if (_pageController.page! < image_post.length - 1) {
+                        _pageController.nextPage(
+                          duration: Duration(milliseconds: 200),
+                          curve: Curves.easeIn,
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ],
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Brand: ${postData['brand']}'),
+          title: Text("ประวัติการโพสต์"),
           toolbarHeight: 40,
           centerTitle: true,
           flexibleSpace: Container(
@@ -61,41 +115,55 @@ class _ShowDetailAllState extends State<ShowDetailAll> {
                 SizedBox(
                   height: 10,
                 ),
-                Text(
-                  "หมายเลขการโพสต์ /0001",
-                  style: TextStyle(fontSize: 18),
-                ),
-                Image.asset("assets/images/shoes.png"),
+                _buildImageSlider(),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      "วันที่ 8/8/2566 เวลา 12:00 น.",
+                      'หมายเลขโพสต์ : ${postData['post_uid']}',
                       style: TextStyle(fontSize: 20),
                     ),
                     Text(
-                      "ชื่อผู้โพสต์ : Pramepree",
+                      "ชื่อผู้โพสต์ :  ${postData['username']}",
                       style: TextStyle(fontSize: 20),
                     ),
                     Text(
-                      "ชื่อสิ่งของ :  รองเท้า",
+                      "วันที่โพสต์ : ${postData['date']}",
                       style: TextStyle(fontSize: 20),
                     ),
                     Text(
-                      "หมวดหมู่ : ของใช้ส่วนตัว",
+                      "เวลาโพสต์ : ${postData['time']}",
                       style: TextStyle(fontSize: 20),
                     ),
                     Text(
-                      "รุ่น : Superstar",
+                      "รายละเอียด : ${postData['detail']}",
                       style: TextStyle(fontSize: 20),
                     ),
-                    Text(
-                      "รายละเอียด : ไซส์ 40 สภาพการใช้งาน 50 % มีรอยถอกตรงส้นเท้า",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Text(
-                      "สถานที่แลกเปลี่ยน : BTS อโศก",
-                      style: TextStyle(fontSize: 20),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 2, right: 15, top: 10, bottom: 10),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width -
+                            17, // Subtract 17 to account for padding and margin
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 214, 214, 212),
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment
+                              .start, // Align text to the left
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 10), // Add left padding to the text
+                              child: Text(
+                                "สถานที่แลกเปลี่ยน : BTS อโศก",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                     SizedBox(
                       height: 10,
@@ -166,24 +234,5 @@ class _ShowDetailAllState extends State<ShowDetailAll> {
         ),
       ),
     );
-    // Scaffold(
-    //   appBar: AppBar(
-    //     title: Text('Post Details'),
-    //   ),
-    //   body: postData.isNotEmpty
-    //       ? SingleChildScrollView(
-    //           child: Column(
-    //             crossAxisAlignment: CrossAxisAlignment.start,
-    //             children: <Widget>[
-    //               Text('Brand: ${postData['brand']}'),
-    //               Text('Date: ${postData['date']}'),
-    //               Text('Detail: ${postData['detail']}'),
-    //               // Add more widgets for each piece of data you want to display
-    //               // You can also add an image using Image.network if 'imageUrls' is available
-    //             ],
-    //           ),
-    //         )
-    //       : Center(child: CircularProgressIndicator()),
-    // );
   }
 }
