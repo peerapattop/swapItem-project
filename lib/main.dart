780bc29a3e6034db9ec95662e3611d3396a01625
pramepree <pramepree159@gmail.com>
2023-12-14 21:22:16 +0700

    backup แล้ว

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: MainPage(), //555
     );
   }
 }
