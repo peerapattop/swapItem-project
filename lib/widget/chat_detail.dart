aa4648f7fff61c46c880c60b1832e4b175ccf8fd
peerapattop <peerapatza401@gmail.com>
2023-12-19 22:50:11 +0700

    แก้ไขเพิ่มข้อมูลแชท ไปยัง realtimedatabase

--- lib/widget/chat_detail.dart
+++ lib/widget/chat_detail.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:swapitem/widget/chat.dart';
 
 class ChatDetail extends StatefulWidget {
   final String username;
@@ -19,18 +17,28 @@ class _ChatDetailState extends State<ChatDetail> {
   User? get currentUser => FirebaseAuth.instance.currentUser;
   String currentUserUsername = '';
 
+  DatabaseReference get userMessagesRef {
+    var user = currentUser;
+    if (user != null) {
+      return FirebaseDatabase.instance
+          .ref()
+          .child('users/${user.uid}/messages');
+    }
+    throw Exception('User not authenticated');
+  }
+
   @override
   void initState() {
     super.initState();
     username = widget.username;
-    getCurrentUsername(); // Call the method to set up the listener
+    getCurrentUsername();
   }
 
   void getCurrentUsername() {
     var user = currentUser;
     if (user != null) {
       DatabaseReference userRef =
-          FirebaseDatabase.instance.ref('users/${user.uid}/username');
+          FirebaseDatabase.instance.ref().child('users/${user.uid}/username');
       userRef.onValue.listen((event) {
         final String usernameme = event.snapshot.value as String;
         setState(() {
@@ -40,86 +48,60 @@ class _ChatDetailState extends State<ChatDetail> {
     }
   }
 
-  Stream<QuerySnapshot> getMessagesStream() {
-    return FirebaseFirestore.instance
-        .collection('chats')
-        .orderBy('timestamp')
-        .snapshots();
-  }
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(70.0),
-          child: Padding(
-            padding: EdgeInsets.only(top: 1),
-            child: AppBar(
-              elevation: 20, // ปรับค่านี้ตามต้องการ
-              leadingWidth: 30,
-              title: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(35),
-                    child: Image.asset(
-                      'assets/images/pramepree.png',
-                      height: 45,
-                      width: 45,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(username),
-                ],
-              ),
-            ),
-          ),
+        appBar: AppBar(
+          title: Text(username),
         ),
         body: StreamBuilder(
-            stream: getMessagesStream(),
-            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (!snapshot.hasData) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              List<QueryDocumentSnapshot> messages = snapshot.data!.docs;
-              return ListView.builder(
-                padding: EdgeInsets.only(top: 20, left: 20, right: 20),
-                itemCount: messages.length,
-                itemBuilder: (context, index) {
-                  var message = messages[index].data() as Map<String, dynamic>;
-                  String text = message['text'];
-                  String sender = message['sender'];
-
-                  return Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    child: Row(
-                      mainAxisAlignment: sender == currentUserUsername
-                          ? MainAxisAlignment.end
-                          : MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            color: sender == currentUserUsername
-                                ? Colors.blue
-                                : Colors.green,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Text(
-                            text,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
+          stream: userMessagesRef.child(username).onValue,
+          builder: (context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
               );
-            }),
+            }
+
+            Map<dynamic, dynamic> messages =
+                snapshot.data!.snapshot.value ?? {};
+
+            return ListView.builder(
+              padding: EdgeInsets.all(20),
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                var message = messages.values.elementAt(index);
+                String text = message['text'];
+                String sender = message['sender'];
+
+                return Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: sender == currentUserUsername
+                        ? MainAxisAlignment.end
+                        : MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: sender == currentUserUsername
+                              ? Colors.blue
+                              : Colors.green,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text(
+                          text,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ),
         bottomSheet: _buildBottomSheet(),
       ),
     );
@@ -167,13 +149,18 @@ class _ChatDetailState extends State<ChatDetail> {
   void _sendMessage(String currentUserUsername) {
     String messageText = _controller.text.trim();
     if (messageText.isNotEmpty && currentUserUsername.isNotEmpty) {
-      FirebaseFirestore.instance.collection('chats').add({
-        'text': messageText,
-        'sender': currentUserUsername,
-        'recipient': username,
-        'timestamp': FieldValue.serverTimestamp(),
-      });
-      _controller.clear();
+      userMessagesRef
+          .child(username)
+          .push()
+          .set({
+            'text': messageText,
+            'sender': currentUserUsername,
+            'timestamp': ServerValue.timestamp,
+          })
+          .then((_) => _controller.clear())
+          .catchError((error) {
+            print("Failed to send message: $error");
+          });
     }
   }
 }
