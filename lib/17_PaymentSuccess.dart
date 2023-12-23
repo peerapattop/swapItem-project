9595504bf55b2a872e3431a3e34dbc897917db5c
peerapattop <peerapatza401@gmail.com>
2023-12-23 13:04:37 +0700

    เพิ่มปุ่มไปหน้าแรก

--- lib/17_PaymentSuccess.dart
+++ lib/17_PaymentSuccess.dart
@@ -1,16 +1,18 @@
 import 'package:intl/intl.dart';
-
+import 'package:swapitem/2_home_page.dart';
 import '18_HistoryPayment.dart';
 import 'package:flutter/material.dart';
-//ชำระเงินสำเร็จ
 
-// ignore: must_be_immutable
+//หน้าชำระเงินสำเร็จ
 class PaymentSuccess extends StatelessWidget {
   DateTime date;
   DateTime time;
 
-  PaymentSuccess(
-      {required this.date, required this.time,});
+  PaymentSuccess({
+    super.key,
+    required this.date,
+    required this.time,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +23,15 @@ class PaymentSuccess extends StatelessWidget {
           toolbarHeight: 40,
           centerTitle: true,
           flexibleSpace: Container(
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('assets/images/image 40.png'),
                 fit: BoxFit.fill,
               ),
             ),
           ),
+          automaticallyImplyLeading:
+              false, // ตั้งค่าเป็น false เพื่อไม่แสดงปุ่ม "Back"
         ),
         body: Center(
           child: Column(
@@ -40,45 +44,59 @@ class PaymentSuccess extends StatelessWidget {
                   height: 100,
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
+              const SizedBox(height: 20),
+              const Text(
                 "ชำระเงินสำเร็จ",
                 style: TextStyle(fontSize: 20, color: Colors.green),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "รอตรวจสอบที่หน้าประวัติการชำระเงิน",
+              const SizedBox(height: 20),
+              const Text(
+                "ตรวจสอบสถานะที่หน้าประวัติการชำระเงิน",
                 style: TextStyle(fontSize: 20),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              
-              SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 40),
               Text(
                 DateFormat('วันที่ d MMMM yyyy', 'th').format(date),
-                style: TextStyle(fontSize: 20),
+                style: const TextStyle(fontSize: 20),
               ),
               Text(
                 DateFormat('เวลา HH:mm น.', 'th').format(time),
-                style: TextStyle(fontSize: 20),
+                style: const TextStyle(fontSize: 20),
               ),
-              SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => HistoryPayment()),
-                    );
-                  },
-                  child: Text("ประวัติการชำระเงิน")),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => HistoryPayment()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, // กำหนดสีพื้นหลังเป็นสีฟ้า
+                ),
+                child: const Text(
+                  "ประวัติการชำระเงิน",
+                  style: TextStyle(
+                      color: Colors.white,fontSize: 20), // กำหนดสีของข้อความเป็นสีขาว
+                ),
+              ),
+              const SizedBox(height: 50),
+              TextButton.icon(
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.home,
+                  color: Colors.blue,
+                ),
+                label: const Text(
+                  'หน้าแรก',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
             ],
           ),
         ),
