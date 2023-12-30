8b51be7ff1daa5f524a43d673740132927bbe004
peerapattop <peerapatza401@gmail.com>
2023-12-30 15:22:15 +0700

    เพิ่มระบบรับการแจ้งเตือน

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -14,6 +15,8 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await FirebaseApi().initNotifications();
+  await FirebaseMessaging.instance.subscribeToTopic('all');
+
   runApp(MyApp());
 }
 
