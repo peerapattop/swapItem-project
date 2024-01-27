f3d1df49602ec15dbeeaef40fb6cfe37ca8fa6a3
peerapattop <peerapatza401@gmail.com>
2024-01-27 21:37:40 +0700

    แสดงโพสต์ที่เป็น vip ก่อน

--- lib/widget/grid_view.dart
+++ lib/widget/grid_view.dart
@@ -65,256 +65,146 @@ class _ShowAllPostItemState extends State<ShowAllPostItem> {
                       .contains(widget.searchString!.toLowerCase()))
               .toList();
 
-          return Column(
-            children: <Widget>[
-              if (widget.searchString == null || widget.searchString!.isEmpty)
-                Expanded(
-                  child: GridView.builder(
-                    padding: const EdgeInsets.only(
-                      left: 3,
-                      right: 3,
-                      top: 3,
-                      bottom: 150,
-                    ),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 3 / 5.5,
-                      crossAxisSpacing: 3,
-                      mainAxisSpacing: 3,
-                    ),
-                    itemCount: dataMap.length,
-                    itemBuilder: (context, index) {
-                      dynamic userData = dataMap.values.elementAt(index);
-                      String item_name = userData['item_name'].toString();
-                      String item_name1 = userData['item_name1'].toString();
-                      String post_uid = userData['post_uid'].toString();
-                      String lati = userData['latitude'].toString();
-                      String longti = userData['longitude'].toString();
-                      String imageUser = userData['imageUser'];
-                     bool isVip = userData['status_user'] == 'ผู้ใช้พรีเมี่ยม';
+          filteredData.sort((a, b) {
+            String statusA =
+                a['status_user'] ?? ''; // Use an empty string if status is null
+            String statusB =
+                b['status_user'] ?? ''; // Use an empty string if status is null
 
+            // Prioritize 'ผู้ใช้พรีเมี่ยม' cards first
+            if (statusA == 'ผู้ใช้พรีเมี่ยม' && statusB != 'ผู้ใช้พรีเมี่ยม') {
+              return -1; // Move card A (VIP) to the front
+            } else if (statusA != 'ผู้ใช้พรีเมี่ยม' &&
+                statusB == 'ผู้ใช้พรีเมี่ยม') {
+              return 1; // Move card B (VIP) to the front
+            } else {
+              return 0; // No priority change for other cases
+            }
+          });
 
-                      List<String> imageUrls =
-                          List<String>.from(userData['imageUrls'] ?? []);
-                      return Card(
-                        clipBehavior: Clip.antiAlias,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        elevation: 5,
-                        margin: const EdgeInsets.all(10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Center(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: [
-                                    if (isVip)
-                                      Image.asset('assets/images/vip.png'),
-                                    Text(
-                                      'ชื่อสิ่งของ: $item_name',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            if (imageUrls.isNotEmpty)
-                              Center(
-                                child: AspectRatio(
-                                  aspectRatio: 1 / 1,
-                                  child: Image.network(
-                                    imageUrls.first,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
-                              child: Center(
-                                child: Text(
-                                  'แลกเปลี่ยนกับ',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color:
-                                        const Color.fromARGB(255, 22, 22, 22),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 6),
-                              child: Center(
-                                child: Text(
-                                  '$item_name1',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.grey[600],
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Spacer(),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  Future.delayed(Duration(seconds: 1), () {
-                                    Navigator.of(context).push(
-                                      MaterialPageRoute(
-                                        builder: (context) => ShowDetailAll(
-                                          postUid: post_uid,
-                                          longti: longti,
-                                          lati: lati,
-                                          imageUser: imageUser,
-                                        ),
-                                      ),
-                                    );
-                                  });
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                      Theme.of(context).primaryColor,
-                                  foregroundColor: Colors.white,
-                                ),
-                                child: Center(child: Text('รายละเอียด')),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
+          return Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.only(
+                left: 3,
+                right: 3,
+                top: 3,
+                bottom: 150,
+              ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 5.5,
+                crossAxisSpacing: 3,
+                mainAxisSpacing: 3,
+              ),
+              itemCount: filteredData.length,
+              itemBuilder: (context, index) {
+                dynamic userData = filteredData[index];
+                String item_name = userData['item_name'].toString();
+                String item_name1 = userData['item_name1'].toString();
+                String post_uid = userData['post_uid'].toString();
+                String lati = userData['latitude'].toString();
+                String longti = userData['longitude'].toString();
+                String imageUser = userData['imageUser'];
+                bool isVip = userData['status_user'] == 'ผู้ใช้พรีเมี่ยม';
+
+                List<String> imageUrls =
+                    List<String>.from(userData['imageUrls'] ?? []);
+                return Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                ),
-              // Display only if there is a search
-              if (widget.searchString != null &&
-                  widget.searchString!.isNotEmpty)
-                Expanded(
-                  child: GridView.builder(
-                    padding: const EdgeInsets.only(
-                      left: 3,
-                      right: 3,
-                      top: 3,
-                      bottom: 150,
-                    ),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 3 / 5.5,
-                      crossAxisSpacing: 3,
-                      mainAxisSpacing: 3,
-                    ),
-                    itemCount: filteredData.length,
-                    itemBuilder: (context, index) {
-                      dynamic userData = filteredData[index];
-                      String item_name = userData['item_name'].toString();
-                      String item_name1 = userData['item_name1'].toString();
-                      String post_uid = userData['post_uid'].toString();
-                      String lati = userData['latitude'].toString();
-                      String longti = userData['longitude'].toString();
-                      String imageUser = userData['imageUser'];
-                      List<String> imageUrls =
-                          List<String>.from(userData['imageUrls'] ?? []);
-                      return Card(
-                        clipBehavior: Clip.antiAlias,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                  elevation: 5,
+                  margin: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              if (isVip)
+                                Image.asset('assets/images/vip.png'),
+                              Text(
+                                'ชื่อสิ่งของ: $item_name',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                        elevation: 5,
-                        margin: const EdgeInsets.all(10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Center(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'ชื่อสิ่งของ: $item_name',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
+                      ),
+                      if (imageUrls.isNotEmpty)
+                        Center(
+                          child: AspectRatio(
+                            aspectRatio: 1 / 1,
+                            child: Image.network(
+                              imageUrls.first,
+                              fit: BoxFit.cover,
                             ),
-                            if (imageUrls.isNotEmpty)
-                              Center(
-                                child: AspectRatio(
-                                  aspectRatio: 1 / 1,
-                                  child: Image.network(
-                                    imageUrls.first,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                            SizedBox(height: 10),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
-                              child: Center(
-                                child: Text(
-                                  'แลกเปลี่ยนกับ',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color:
-                                        const Color.fromARGB(255, 22, 22, 22),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 6),
-                              child: Center(
-                                child: Text(
-                                  '$item_name1',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.grey[600],
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Spacer(),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  Future.delayed(Duration(seconds: 1), () {
-                                    Navigator.of(context).push(
-                                      MaterialPageRoute(
-                                        builder: (context) => ShowDetailAll(
-                                          postUid: post_uid,
-                                          longti: longti,
-                                          lati: lati,
-                                          imageUser: imageUser,
-                                        ),
-                                      ),
-                                    );
-                                  });
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                      Theme.of(context).primaryColor,
-                                  foregroundColor: Colors.white,
-                                ),
-                                child: Center(child: Text('Details')),
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
-                      );
-                    },
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Center(
+                          child: Text(
+                            'แลกเปลี่ยนกับ',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: const Color.fromARGB(255, 22, 22, 22),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 6),
+                        child: Center(
+                          child: Text(
+                            '$item_name1',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Future.delayed(Duration(seconds: 1), () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => ShowDetailAll(
+                                    postUid: post_uid,
+                                    longti: longti,
+                                    lati: lati,
+                                    imageUser: imageUser,
+                                  ),
+                                ),
+                              );
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Theme.of(context).primaryColor,
+                            foregroundColor: Colors.white,
+                          ),
+                          child: Center(child: Text('รายละเอียด')),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-            ],
+                );
+              },
+            ),
           );
         },
       ),
