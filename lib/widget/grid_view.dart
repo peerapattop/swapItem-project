6d4c634ab0057e4a4cc18ef34ca4e7e4677f429a
peerapattop <peerapatza401@gmail.com>
2024-02-08 22:38:34 +0700

    ปรับเปลี่ยน ui

--- lib/widget/grid_view.dart
+++ lib/widget/grid_view.dart
@@ -22,7 +22,7 @@ class _GridView2State extends State<GridView2> {
         stream: _postRef.onValue,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
+            return const Center(
               child: CircularProgressIndicator(),
             );
           } else if (snapshot.hasError) {
@@ -30,7 +30,7 @@ class _GridView2State extends State<GridView2> {
               child: Text('Error: ${snapshot.error}'),
             );
           } else if (!snapshot.hasData || snapshot.data == null) {
-            return Center(
+            return const Center(
               child: Text("No data available"),
             );
           }
@@ -43,26 +43,27 @@ class _GridView2State extends State<GridView2> {
 
             filteredData = dataMap.values
                 .where((userData) =>
-            widget.searchString == null ||
-                widget.searchString!.isEmpty ||
-                userData['item_name']
-                    .toString()
-                    .toLowerCase()
-                    .contains(widget.searchString!.toLowerCase()) ||
-                userData['item_name1']
-                    .toString()
-                    .toLowerCase()
-                    .contains(widget.searchString!.toLowerCase()))
+                    widget.searchString == null ||
+                    widget.searchString!.isEmpty ||
+                    userData['item_name']
+                        .toString()
+                        .toLowerCase()
+                        .contains(widget.searchString!.toLowerCase()) ||
+                    userData['item_name1']
+                        .toString()
+                        .toLowerCase()
+                        .contains(widget.searchString!.toLowerCase()))
                 .toList();
 
             filteredData.sort((a, b) {
-              String statusA =
-                  a['status_user'] ?? ''; // Use an empty string if status is null
-              String statusB =
-                  b['status_user'] ?? ''; // Use an empty string if status is null
+              String statusA = a['status_user'] ??
+                  ''; // Use an empty string if status is null
+              String statusB = b['status_user'] ??
+                  ''; // Use an empty string if status is null
 
               // Prioritize 'ผู้ใช้พรีเมี่ยม' cards first
-              if (statusA == 'ผู้ใช้พรีเมี่ยม' && statusB != 'ผู้ใช้พรีเมี่ยม') {
+              if (statusA == 'ผู้ใช้พรีเมี่ยม' &&
+                  statusB != 'ผู้ใช้พรีเมี่ยม') {
                 return -1; // Move card A (VIP) to the front
               } else if (statusA != 'ผู้ใช้พรีเมี่ยม' &&
                   statusB == 'ผู้ใช้พรีเมี่ยม') {
@@ -74,7 +75,7 @@ class _GridView2State extends State<GridView2> {
 
             return GridView.builder(
               padding: const EdgeInsets.all(10),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 childAspectRatio: 3 / 6.5,
                 crossAxisSpacing: 10,
@@ -83,17 +84,17 @@ class _GridView2State extends State<GridView2> {
               itemCount: filteredData.length,
               itemBuilder: (context, index) {
                 dynamic userData = filteredData[index];
-                String item_name = userData['item_name'].toString();
-                String item_name1 = userData['item_name1'].toString();
-                String post_uid = userData['post_uid'].toString();
-                String lati = userData['latitude'].toString();
-                String longti = userData['longitude'].toString();
+                String itemName = userData['item_name'].toString();
+                String itemName1 = userData['item_name1'].toString();
+                String postUid = userData['post_uid'].toString();
+                String latitude = userData['latitude'].toString();
+                String longitude = userData['longitude'].toString();
                 String imageUser = userData['imageUser'];
                 String userUid = userData['uid'];
                 bool isVip = userData['status_user'] == 'ผู้ใช้พรีเมี่ยม';
 
                 List<String> imageUrls =
-                List<String>.from(userData['imageUrls'] ?? []);
+                    List<String>.from(userData['imageUrls'] ?? []);
                 return Card(
                   clipBehavior: Clip.antiAlias,
                   shape: RoundedRectangleBorder(
@@ -108,11 +109,12 @@ class _GridView2State extends State<GridView2> {
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               if (isVip) Image.asset('assets/images/vip.png'),
                               Text(
-                                'ชื่อสิ่งของ: $item_name',
-                                style: TextStyle(
+                                itemName,
+                                style: const TextStyle(
                                   fontSize: 16,
                                   fontWeight: FontWeight.bold,
                                 ),
@@ -131,17 +133,15 @@ class _GridView2State extends State<GridView2> {
                             ),
                           ),
                         ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      const SizedBox(height: 10),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
                         child: Center(
                           child: Text(
                             'แลกเปลี่ยนกับ',
                             style: TextStyle(
                               fontSize: 14,
-                              color: const Color.fromARGB(255, 22, 22, 22),
+                              color: Color.fromARGB(255, 22, 22, 22),
                             ),
                           ),
                         ),
@@ -150,7 +150,7 @@ class _GridView2State extends State<GridView2> {
                         padding: const EdgeInsets.only(top: 6),
                         child: Center(
                           child: Text(
-                            '$item_name1',
+                            itemName1,
                             style: TextStyle(
                               fontSize: 14,
                               color: Colors.grey[600],
@@ -158,43 +158,44 @@ class _GridView2State extends State<GridView2> {
                           ),
                         ),
                       ),
-                      Spacer(),
+                      const Spacer(),
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: user?.uid != userUid
                             ? ElevatedButton(
-                          onPressed: () {
-                            Future.delayed(Duration(seconds: 1), () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => ShowDetailAll(
-                                    postUid: post_uid,
-                                    longti: longti,
-                                    lati: lati,
-                                    imageUser: imageUser,
+                                onPressed: () {
+                                  Future.delayed(const Duration(seconds: 1),
+                                      () {
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) => ShowDetailAll(
+                                          postUid: postUid,
+                                          longti: longitude,
+                                          lati: latitude,
+                                          imageUser: imageUser,
+                                        ),
+                                      ),
+                                    );
+                                  });
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      Theme.of(context).primaryColor,
+                                  foregroundColor: Colors.white,
+                                ),
+                                child: const Center(child: Text('รายละเอียด')),
+                              )
+                            : const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Center(
+                                  child: Text(
+                                    'โพสต์ของฉัน',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
                                   ),
                                 ),
-                              );
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                            Theme.of(context).primaryColor,
-                            foregroundColor: Colors.white,
-                          ),
-                          child: Center(child: Text('รายละเอียด')),
-                        )
-                            : const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Center(
-                            child: Text(
-                              'โพสต์ของฉัน',
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
+                              ),
                       ),
                     ],
                   ),
@@ -202,7 +203,7 @@ class _GridView2State extends State<GridView2> {
               },
             );
           } else {
-            return Center(
+            return const Center(
               child: Text("No data available"),
             );
           }
