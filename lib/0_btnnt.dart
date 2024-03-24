7633c217250794bfb9c961120c16ee2e3385774e
peerapattop <peerapatza401@gmail.com>
2024-03-24 12:33:41 +0700

    update ui notification

--- lib/0_btnnt.dart
+++ lib/0_btnnt.dart
@@ -4,6 +4,8 @@ import '9_my_profile.dart';
 import 'home_page.dart';
 import 'package:flutter/material.dart';
 
+import 'notification_page.dart';
+
 class btnnt extends StatefulWidget {
   const btnnt({super.key});
 
@@ -22,33 +24,44 @@ class _btnntState extends State<btnnt> {
   static const List<Widget> _widgetOptions = <Widget>[
     HomePage(),
     ChatHomePage(),
+    NotificationD(),
     Profile(),
   ];
+
+  static const List<BottomNavigationBarItem> _bottomNavBarItems = [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'หน้าแรก',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.chat),
+      label: 'แชท',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.notification_add),
+      label: 'แจ้งเตือน',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.person),
+      label: 'ข้อมูลส่วนตัว',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: _widgetOptions.elementAt(_selectedIndex),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'หน้าแรก',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat),
-              label: 'แชท',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'ข้อมูลส่วนตัว',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.amber[800],
-          onTap: _onItemTapped,
-        ),
+    return Scaffold(
+      body: SafeArea(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _bottomNavBarItems,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
       ),
     );
   }
 }
+
+
