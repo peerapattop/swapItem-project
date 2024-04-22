fd841b1dd417a6da3c2e0c74dcf635bf81f620a7
peerapattop <peerapatza401@gmail.com>
2024-04-22 19:17:00 +0700

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -146,6 +146,7 @@ class _HomePageState extends State<HomePage> {
                   setState(() {
                     searchController.clear();
                     _searchString = '';
+                    widget.filter?.clear();
                   });
                 },
               ),
